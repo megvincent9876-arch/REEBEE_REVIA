72cee889b43337ffe2a1b4fccbf20cb65cd015e9
Blessing Robert <megvincent9876@gmail.com>
2026-09-24 20:23:59 -0500

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -147,7 +147,7 @@ app.use((error, req, res, next) => {
     error: error.message,
   });
 });
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
